2fb14aa8b8174b0e74ab769054cb6f3edfe42f06
Leopold Johannes Meinel <leo@meinel.dev>
2025-10-09 09:33:48 +0200

    Example: Add hello world C program

---
+++ 2025-10-09/main.cpp
@@ -0,0 +1,16 @@
+/*
+ * File: main.cpp
+ * Author: Leopold Meinel (leo@meinel.dev)
+ * -----
+ * Copyright (c) 2025 Leopold Meinel & contributors
+ * SPDX ID: MIT
+ * URL: https://opensource.org/licenses/MIT
+ * -----
+ */
+
+#include <cstdio>
+
+int main(int argc, char *argv[]) {
+  std::printf("Hello World\n");
+  return 0;
+}
