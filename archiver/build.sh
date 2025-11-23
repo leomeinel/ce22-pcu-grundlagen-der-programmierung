31604618b9f97a2521149eb373923fec0a3d2381
Leopold Johannes Meinel <leo@meinel.dev>
2025-11-23 17:50:12 +0100

    CMake: Use Ninja

--- archiver/build.sh
+++ archiver/build.sh
@@ -17,5 +17,5 @@ SCRIPT_DIR="$(dirname -- "$(readlink -f -- "${0}")")"
 # Out of source build
 mkdir "${SCRIPT_DIR}"/build || true
 cd "${SCRIPT_DIR}"/build
-cmake "${SCRIPT_DIR}"
+cmake -G Ninja "${SCRIPT_DIR}"
 cmake --build "${SCRIPT_DIR}"/build
